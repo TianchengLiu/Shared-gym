7727b5215027376b2b51db83d94bb5a08be699c6
MinRam <997264157@qq.com>
2018-07-23 22:16:43 +0800

    Temp

--- AndroidClient/app/src/main/java/a210team/sharedgym/MainActivity.java
+++ AndroidClient/app/src/main/java/a210team/sharedgym/MainActivity.java
@@ -3,11 +3,18 @@ package a210team.sharedgym;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // butter knife injection
+        ButterKnife.bind(this);
     }
 }
