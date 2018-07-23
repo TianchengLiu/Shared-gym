490dc45516f31b884af018e5d07a807e411450ba
TianchengLiu <38563992+TianchengLiu@users.noreply.github.com>
2018-07-23 22:15:12 +0800

    状态栏半透明
    半透明

--- AndroidClient/app/src/main/java/a210team/sharedgym/MainActivity.java
+++ AndroidClient/app/src/main/java/a210team/sharedgym/MainActivity.java
@@ -1,13 +1,28 @@
 package a210team.sharedgym;
 
+import android.os.Build;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
